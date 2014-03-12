3cfdfd365ea50327ca9306c1f8116e4fe36e236b
johndigger <johndigger@no-core.de>
2014-03-12 23:26:16 +0100

    packages and mcmod.info created

---
+++ ethernallife_common/ethernallife/EthernalLife.java
@@ -0,0 +1,28 @@
+package ethernallife;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import ethernallife.lib.Reference;
+
+@Mod( modid=Reference.MOD_ID, name=Reference.MOD_NAME,version=Reference.VERSION)
+public class EthernalLife {
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+        
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event){
+        
+    }
+    
+    @EventHandler
+    public void posInit(FMLPostInitializationEvent event){
+        
+    }
+    
+}
